59300dbbfcde02a12e7f4d520cf2ffed836e9645
Denis <denis_vsalda@mail.ru>
2024-05-18 22:25:14 +0500

    master: Правка вёрстки

--- public/js/main/aboutUs.js
+++ public/js/main/aboutUs.js
@@ -1,12 +1,19 @@
 $(function() {
-    $('.about_us .direction a, .about_us_mobile .direction a').on('click', function() {
+    var matchMedia = window.matchMedia('(max-width: 768px)');
+    var serviceId = matchMedia.matches ? `#services_mobile` : `#services`;
+
+    $('.about_us .direction a, .about_us_mobile .direction a').on('click', function(e) {
+        e.stopPropagation();
+
+        console.log(serviceId + (matchMedia.matches ? ` [data-groupname=${$(this).data('groupname')}]` : ''));
+
         $('html, body').animate({
-            scrollTop: $(`#services`).offset().top - window.innerHeight * 0.1
+            scrollTop: $(serviceId + (matchMedia.matches ? ` [data-groupname=${$(this).data('groupname')}]` : '')).offset().top - window.innerHeight * 0.1
         }, {
             duration: 1200,
             easing: "easeInOutCubic",
         });
 
-        $('#services [data-groupname=' + $(this).data('groupname') + ']').trigger('click');
+        $(serviceId + ' [data-groupname=' + $(this).data('groupname') + ']').trigger('click');
     });
 });
